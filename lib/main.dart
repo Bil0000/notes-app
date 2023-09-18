e07b55d22cbf331296d35dd5d3cc2d4368d7ade7
Bil0000 <bilal.bakr.elsherif@gmail.com>
2023-09-18 18:51:50 +0300

    solved all bugs and made more better slide for edit and delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, unused_local_variable
-
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:uuid/uuid.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Generate a unique device ID (UUID)
-  final deviceID = Uuid().v4();
+  final deviceID = const Uuid().v4();
 
   await Supabase.initialize(
-    url: 'Your supabase url',
+    url: 'https://nznhwuyoynclfhwcjtic.supabase.co',
     anonKey:
-        'Your supabase anon key', // Replace with your Supabase anonymous key
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56bmh3dXlveW5jbGZod2NqdGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ3OTYwNDEsImV4cCI6MjAxMDM3MjA0MX0.V9iwmM_3KZ4stzAPGPK7ocjqvoMSNU4285xey28Igc8',
   );
 
   runApp(MyApp(deviceID: deviceID));
@@ -51,13 +51,54 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+
+    // Load filtered notes from local storage when the app starts
+    _loadFilteredNotes();
+
+    // Start listening to the Supabase stream to keep the notes updated
     _notesStream.listen((data) {
       setState(() {
-        // Filter notes based on deviceID
-        notes =
+        // Combine the filtered notes from local storage with new data from the stream
+        final newNotes =
             data.where((note) => note['device_id'] == widget.deviceID).toList();
+
+        for (final newNote in newNotes) {
+          // Check if the note with the same 'id' exists in the list
+          final existingNoteIndex =
+              notes.indexWhere((note) => note['id'] == newNote['id']);
+
+          if (existingNoteIndex != -1) {
+            // Update the existing note if it exists
+            notes[existingNoteIndex] = newNote;
+          } else {
+            // Add the new note if it doesn't exist
+            notes.add(newNote);
+          }
+        }
+
+        _saveFilteredNotes(notes); // Save the combined notes to local storage
       });
     });
+  }
+
+  // Load filtered notes from local storage
+  Future<void> _loadFilteredNotes() async {
+    final prefs = await SharedPreferences.getInstance();
+    final filteredNotesJson = prefs.getString('filtered_notes');
+    if (filteredNotesJson != null) {
+      final filteredNotes = json.decode(filteredNotesJson) as List;
+      setState(() {
+        notes = filteredNotes.cast<Map<String, dynamic>>();
+      });
+    }
+  }
+
+  // Save filtered notes to local storage
+  Future<void> _saveFilteredNotes(
+      List<Map<String, dynamic>> filteredNotes) async {
+    final prefs = await SharedPreferences.getInstance();
+    final filteredNotesJson = json.encode(filteredNotes);
+    await prefs.setString('filtered_notes', filteredNotesJson);
   }
 
   @override
@@ -66,83 +107,97 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('My Notes'),
       ),
-      body: ListView.builder(
-        itemCount: notes.length,
-        itemBuilder: (context, index) {
-          String editedNoteText = notes[index]['body'];
+      body: StreamBuilder<List<Map<String, dynamic>>>(
+        stream: _notesStream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-          return Dismissible(
-            key: UniqueKey(),
-            background: Container(
-              color: Colors.red,
-              alignment: Alignment.centerLeft,
-              padding: const EdgeInsets.only(right: 20),
-              child: const Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-            ),
-            secondaryBackground: Container(
-              color: Colors.green,
-              alignment: Alignment.centerRight,
-              padding: const EdgeInsets.only(left: 20),
-              child: const Icon(
-                Icons.edit,
-                color: Colors.white,
-              ),
-            ),
-            onDismissed: (direction) async {
-              if (direction == DismissDirection.endToStart) {
-                String editedNoteText = notes[index]['body'];
-                final updatedNote = await showDialog(
-                  context: context,
-                  builder: ((context) {
-                    return AlertDialog(
-                      title: const Text('Edit Note'),
-                      content: TextFormField(
-                        initialValue: editedNoteText,
-                        onChanged: (value) {
-                          editedNoteText = value;
-                        },
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Cancel'),
-                        ),
-                        TextButton(
-                          onPressed: () async {
-                            Navigator.of(context).pop(editedNoteText);
-                            await Supabase.instance.client
-                                .from('notes')
-                                .update({'body': editedNoteText})
-                                .eq('id', notes[index]['id'])
-                                .execute();
-                          },
-                          child: const Text('Save'),
-                        ),
-                      ],
-                    );
-                  }),
-                );
+          return ListView.builder(
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              String editedNoteText = notes[index]['body'];
 
-                setState(() {
-                  notes[index]['body'] = updatedNote ?? editedNoteText;
-                });
-              } else if (direction == DismissDirection.startToEnd) {
-                final noteId = notes[index]['id'];
-                await Supabase.instance.client
-                    .from('notes')
-                    .delete()
-                    .eq('id', noteId)
-                    .execute();
-              }
+              return Slidable(
+                key: UniqueKey(),
+                startActionPane: ActionPane(
+                  motion: const ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (direction) async {
+                        String editedNoteText = notes[index]['body'];
+                        final updatedNote = await showDialog(
+                          context: context,
+                          builder: ((context) {
+                            return AlertDialog(
+                              title: const Text('Edit Note'),
+                              content: TextFormField(
+                                initialValue: editedNoteText,
+                                onChanged: (value) {
+                                  editedNoteText = value;
+                                },
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    Navigator.of(context).pop(editedNoteText);
+                                    await Supabase.instance.client
+                                        .from('notes')
+                                        .update({'body': editedNoteText})
+                                        .eq('id', notes[index]['id'])
+                                        .execute();
+                                  },
+                                  child: const Text('Save'),
+                                ),
+                              ],
+                            );
+                          }),
+                        );
+
+                        setState(() {
+                          notes[index]['body'] = updatedNote ?? editedNoteText;
+                        });
+                      },
+                      backgroundColor: Colors.green,
+                      icon: Icons.edit,
+                      label: 'Edit Note',
+                    ),
+                  ],
+                ),
+                endActionPane: ActionPane(
+                  motion: const ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (direction) async {
+                        final noteId = notes[index]['id'];
+                        await Supabase.instance.client
+                            .from('notes')
+                            .delete()
+                            .eq('id', noteId)
+                            .execute();
+
+                        setState(() {
+                          notes.removeAt(index);
+                        });
+                      },
+                      backgroundColor: Colors.red,
+                      icon: Icons.delete,
+                      label: 'Delete Note',
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  title: Text(editedNoteText),
+                ),
+              );
             },
-            child: ListTile(
-              title: Text(editedNoteText),
-            ),
           );
         },
       ),
@@ -169,7 +224,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   TextButton(
                     onPressed: () async {
                       if (newNoteText.isNotEmpty) {
-                        // Include the device ID when inserting a new note
                         await Supabase.instance.client.from('notes').insert({
                           'body': newNoteText,
                           'device_id': widget.deviceID
